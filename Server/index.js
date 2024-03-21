5a1b95638e66e5d4ab0178ac1dab9df1eefe420c
Arshvir Singh Kalsi <arshvirsk26@gmail.com>
2024-03-21 15:00:32 +0530

    Hello server commit

--- Server/index.js
+++ Server/index.js
@@ -617,7 +617,7 @@ app.post("/e6check", async (req, res) => {
 //   res.json({ message: "Hello world" });
 // });
 
-server.listen(PORT, () =>
+app.listen(PORT, () =>
     console.log("App running on https://evento-w3o7.onrender.com/")
 );
 
